d0868a5db2080b0a3bc69caf886d2438384a5d43
MatthewKosloski <matthewkosloski@me.com>
2017-06-26 02:11:27 -0500

    updated demo.php

--- demo.php
+++ demo.php
@@ -4,32 +4,9 @@ require_once("metabox_constructor_class.php");
 
 $metabox = new Metabox_Constructor(array(
 	'id' => 'metabox_id',
-	'title' => __('Metabox Title', 'experiment_functionality'),
+	'title' => __('Metabox Demo', 'experiment_functionality'),
 	'post_type' => 'post'
 ));
-
-// $metabox->addText(array(
-// 	'id' => 'metabox_text_field',
-// 	'label' => 'Text Field'
-// ));
-
-// $metabox->addTextArea(array(
-// 	'id' => 'metabox_textarea_field',
-// 	'desc' => 'Some description text for a textarea.', 
-// 	'label' => 'Textarea Field'
-// ));
-
-// $metabox->addCheckbox(array(
-// 	'id' => 'metabox_checkbox_field',
-// 	'desc' => 'Foobarbax',
-// 	'label' => 'Checkbox Field'
-// ));
-
-// $metabox->addImage(array(
-// 	'id' => 'metabox_image_field2',
-// 	'desc' => 'Upload an image by clicking the button',
-// 	'label' => 'Image Upload'
-// ));
 
 $metabox_repeater_block15_fields[] = $metabox->addText(array(
 	'id' => 'metabox_repeater_text_field',
@@ -43,34 +20,38 @@ $metabox_repeater_block15_fields[] = $metabox->addTextArea(array(
 
 $metabox->addRepeaterBlock(array(
 	'id' => 'metabox_repeater_block15',
-	'desc' => 'These products show up on the products page.',
+	'desc' => 'Repeater blocks can be used to store an array of content with a dynamic length (e.g, Products).',
 	'fields' => $metabox_repeater_block15_fields,
-	'label' => 'Product List',
+	'label' => 'Repeater Block Field',
 	'single_label' => 'Product'
 ));
 
-// $metabox_repeater_block20_fields[] = $metabox->addImage(array(
-// 	'id' => 'metabox_repeater_block20_image',
-// 	'desc' => 'Upload an image by clicking the button',
-// 	'label' => 'Image Upload'
-// ), true);
+$metabox_repeater_block15_fields[] = $metabox->addWysiwyg(array(
+	'id' => 'metabox_wysiwyg_field',
+	'label' => 'WYSIWYG Field',
+	'desc' => 'You can use a WYSIWYG editor to facilitate the management of HTML content.'
+));
 
-// $metabox_repeater_block20_fields[] = $metabox->addCheckbox(array(
-// 	'id' => 'metabox_repeater_block20_checkboz',
-// 	'desc' => 'Foobarbax',
-// 	'label' => 'Checkbox Field'
-// ), true);
+$metabox->addCheckbox(array(
+	'id' => 'metabox_checkbox_field',
+	'desc' => 'Checkboxes are a great way to facilitate conditional logic.',
+	'label' => 'Checkbox Field'
+));
 
-// $metabox->addRepeaterBlock(array(
-// 	'id' => 'metabox_repeater_block20',
-// 	'desc' => 'Repeater block with photos.',
-// 	'fields' => $metabox_repeater_block20_fields,
-// 	'label' => 'Photo Repeater'
-// ));
+$metabox->addImage(array(
+	'id' => 'metabox_image_field2',
+	'desc' => 'Upload an image, or change it, by clicking the button below the preview.',
+	'label' => 'Image Upload Field'
+));
 
-// $metabox_repeater_block15_fields[] = $metabox->addWysiwyg(array(
-// 	'id' => 'metabox_wysiwyg_field',
-// 	'label' => 'Example Wysiwyg Editor'
-// ));
+$metabox->addText(array(
+	'id' => 'metabox_text_field',
+	'label' => 'Text Field'
+));
+
+$metabox->addTextArea(array(
+	'id' => 'metabox_textarea_field',
+	'label' => 'Textarea Field'
+));
 
 ?>
